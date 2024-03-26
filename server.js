7de0c7c7c7ef5c5eaaacd4a1b0cac58154dd1ef4
Aswany Augustine <Aswany.Augustine@ibm.com>
2024-03-26 13:43:34 +0530

    feat: separating frontend and backend

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const cors = require('cors');
 
-
-app.use(cors());
-
-
-// Serve static files
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Config endpoint
 app.get('/config', (req, res) => {
   res.json({ REACT_APP_BACKEND_URL: process.env.REACT_APP_BACKEND_URL });
 });
 
-// Redirect all other requests to index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
